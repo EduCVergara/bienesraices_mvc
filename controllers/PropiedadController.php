dda94a78d6a7bbb418c8466f53545ab94222e3ab
Edu Constancio Vergara <e.constanciovergara@gmail.com>
2025-02-20 01:44:08 -0300

    definiendo rutasPOST en el Router

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -22,6 +22,10 @@ class PropiedadController {
         $propiedad = new Propiedad;
         $vendedores = Vendedores::all();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+
         $router->render('propiedades/crear', [
            'propiedad' => $propiedad,
            'vendedores' => $vendedores
--- public/index.php
+++ public/index.php
@@ -12,8 +12,11 @@ $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
+$router->post('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 
 $router->get('/vendedores/crear', [PropiedadController::class, 'crear']);
+$router->post('/vendedores/crear', [PropiedadController::class, 'crear']);
 $router->get('/vendedores/actualizar', [PropiedadController::class, 'actualizar']);
+$router->post('/vendedores/actualizar', [PropiedadController::class, 'actualizar']);
 
 $router->comprobarRutas();  
